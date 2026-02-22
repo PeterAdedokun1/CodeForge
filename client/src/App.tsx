c2fed43cf7a01fc2f87c20fb27de4886afcb593e
peter-opeyemi <peteradedokun167@gmail.com>
2026-02-22 08:10:07 +0100

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { AppLayout } from './components/AppLayout';
 
 function App() {
+
   const urlParams = new URLSearchParams(window.location.search);
   const role = (urlParams.get('role') || 'patient') as 'patient' | 'chew' | 'hospital';
   return <AppLayout initialRole={role} />;
